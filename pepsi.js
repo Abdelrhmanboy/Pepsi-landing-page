d01a8133327e84445c83cd5c8968eb3ada654004
Abdelrhman Helmy <119740075+Abdelrhmanboy@users.noreply.github.com>
2023-03-13 20:04:43 +0200

    Update pepsi.js

--- pepsi.js
+++ pepsi.js
@@ -5,7 +5,6 @@ let btn = document.getElementById('btn');
 let can1 = document.getElementById('can1');
 let can2 = document.getElementById('can2');
 let can3 = document.getElementById('can3');
-let canT = document.getElementById('canT');
 let face = document.getElementById('F');
 let insta = document.getElementById('I');
 let twiter = document.getElementById('T');
@@ -19,42 +18,6 @@ function colorc(zzz) {
     container.style.background = zzz;
 }
 
-canT.onclick = function(){
-   // functions
-   pepsi(this.src);
-   color('linear-gradient(135deg,#000 20%,#4cb705)')
-   colorc('linear-gradient(135deg,#000 20%,#4cb705)')
-
-   // btn fuctions
-   btn.onmouseenter = function(){
-   btn.style.backgroundColor = '#4cb705';
-   btn.style.color = '#000'
-} ;
-   btn.onmouseleave = function(){
-   btn.style.backgroundColor = '#fff';
-   btn.style.color = '#000'
-}
-face.onmouseenter = function(){
-   face.style.backgroundColor = '#4cb705';
-} ;
-face.onmouseleave = function(){
-   face.style.removeProperty('background-color');
-   
-}
-insta.onmouseenter = function(){
-   insta.style.backgroundColor = '#4cb705';
-} ;
-insta.onmouseleave = function(){
-   insta.style.removeProperty('background-color')
-   
-}
-twiter.onmouseenter = function(){
-   twiter.style.backgroundColor = '#4cb705';
-} ;
-twiter.onmouseleave = function(){
-   twiter.style.removeProperty('background-color')
-}
-}
 
 can1.onclick = function(){
     // functions
